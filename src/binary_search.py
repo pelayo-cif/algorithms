628d6bfc4fd226e9426b2dfedf19ad95e7e2098c
Pelayo Cifuentes <uo271060@uniovi.es>
2023-12-11 11:56:45 +0100

    Binary Search [Completed]

--- src/binary_search.py
+++ src/binary_search.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 class BinarySearch():
     
-    def search(self, nums: list[int], n:int) -> Optional[int]:
+    def search(self, nums: list[int], n:int) -> int | None:
         """ Searchs an element n insede the array nums. 
         Array must be sorted in order to successfully find the number.
 
@@ -11,7 +11,7 @@ class BinarySearch():
             n (int): Element to find.
 
         Returns:
-            Optional[int]: Pos or n inside the array
+            int | None: Pos of n inside the array or None if not found
         """
         # Empty array
         if len(nums) == 0:
@@ -19,6 +19,18 @@ class BinarySearch():
         # Last option
         if len(nums) == 1:
             return 0 if nums[0] == n else None
-        # Filter
-        pos: int = (len(nums) // 2) + 1
-        return pos        
+        # Get element in the middle
+        pos: int = len(nums) // 2
+        # Evaluate next call
+        if nums[pos] == n:
+            return pos
+        elif n < nums[pos]:
+            return self.search(nums[0:pos],n)
+        else:
+            res: int | None = self.search(nums[pos:len(nums)],n)
+            return res + len(nums[0:pos]) if res is not None else None 
+    
+    
+binary_search = BinarySearch()
+res = binary_search.search(nums = [0,1,2,3,4,5,6,7,8,9,10],n = 20)
+print(res)
--- test/binary_search_test.py
+++ test/binary_search_test.py
@@ -6,27 +6,55 @@ sys.path.append(str(pathlib.Path(__file__).parents[1]))
 
 from src.binary_search import BinarySearch
 
+from typing import List
 
 class TestBinarySearch(unittest.TestCase):
 
     def test_empty(self):
         """Test binary search on empty array 
         """
-        binary_search = BinarySearch()
+        binary_search: BinarySearch = BinarySearch()
         self.assertIsNone(binary_search.search(nums = [],n = 1))
     
     def test_length_one_success(self):
         """Test binary search on lenght 1 array, success case
         """
-        binary_search = BinarySearch()
+        binary_search: BinarySearch = BinarySearch()
         self.assertEquals(binary_search.search(nums = [1],n = 1),0)
         
     def test_length_one_fail(self):
         """Test binary search on lenght 1 array, fail case
         """
-        binary_search = BinarySearch()
+        binary_search: BinarySearch = BinarySearch()
         self.assertIsNone(binary_search.search(nums = [2],n = 1))
-
+        
+    def test_length_three_success(self):
+        """Test binary search on lenght 3 array, success case
+        """
+        binary_search: BinarySearch = BinarySearch()
+        nums: List[int] = [0,1,2]
+        for num in nums:
+            self.assertEquals(binary_search.search(nums = nums,n = num),num)
+        
+    def test_length_three_fail(self):
+        """Test binary search on lenght 3 array, fail case
+        """
+        binary_search: BinarySearch = BinarySearch()
+        self.assertIsNone(binary_search.search(nums = [0,1,2],n = 10))
+        
+    def test_length_ten_success(self):
+        """Test binary search on lenght 10 array, success case
+        """
+        binary_search: BinarySearch = BinarySearch()
+        nums: List[int] = [0,1,2,3,4,5,6,7,8,9]
+        for num in nums:
+            self.assertEquals(binary_search.search(nums = nums,n = num),num)
+        
+    def test_length_ten_fail(self):
+        """Test binary search on lenght 10 array, fail case
+        """
+        binary_search: BinarySearch = BinarySearch()
+        self.assertIsNone(binary_search.search(nums = [0,1,2,3,4,5,6,7,8,9],n = 10))
 
 if __name__ == '__main__':
     unittest.main()
